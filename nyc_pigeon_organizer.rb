0052c5703ca442b164898f47c44136f175110cc4
Chris Kakos <ckakos@gmail.com>
2018-12-15 22:57:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,29 +30,29 @@
 # end
 
 # Solution 1:
-def nyc_pigeon_organizer(data)
-    # write your code here!
-    new_hash = {}
-
-    data.each do |property, hash|
-        hash.each do |attribute, array|
-            array.each do |name|
-                if !new_hash.has_key?(name)
-                    new_hash[name] = {}
-                end
-
-                if !new_hash[name].has_key?(property)
-                    new_hash[name][property] = []
-                end
-
-                if !new_hash[name][property].include?(attribute)
-                    new_hash[name][property] << attribute.to_s
-                end
-            end
-        end
-    end
-    new_hash
-end
+# def nyc_pigeon_organizer(data)
+#     # write your code here!
+#     new_hash = {}
+# 
+#     data.each do |property, hash|
+#         hash.each do |attribute, array|
+#             array.each do |name|
+#                 if !new_hash.has_key?(name)
+#                     new_hash[name] = {}
+#                 end
+# 
+#                 if !new_hash[name].has_key?(property)
+#                     new_hash[name][property] = []
+#                 end
+# 
+#                 if !new_hash[name][property].include?(attribute)
+#                     new_hash[name][property] << attribute.to_s
+#                 end
+#             end
+#         end
+#     end
+#     new_hash
+# end
 
 # # Solution 2:
 # def nyc_pigeon_organizer (data)
@@ -100,5 +100,7 @@ end
 #     return final
 # end
 
-
-
+# Solution 3:
+def nyc_pigeon_organizer(data)
+    
+end
